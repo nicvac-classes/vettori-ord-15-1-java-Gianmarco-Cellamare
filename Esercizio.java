09a3cd47daa9bb1bdd16551b76c9cc01f41e66cf
Gianmarco-Cellamare <gianmarco.cellamare@marconibari.edu.it>
2025-04-03 17:08:14 +0000

    Vettori ord. (15.1); Java

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,103 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
 
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
 
-// Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
+    public static void bubblesortparalleli(int[] nomi,int[] posizione ) {
+        int i, j, cont, t , t1;
+        boolean scambio;
+        int n = nomi.length;
+        int n = posizione.length;
+
+        cont = 0;
+        i = 0;
+        scambio = true;
+
+      
+        while (i <= n - 1 && scambio) {
+            scambio = false;
+            j = 0;
+
+      
+            while (j <= n - 2 - i) {
+                if (nomi[j] > nomi[j+1]) {
+                    t = nomi[j];
+                    nomi[j] = nomi[j + 1];
+                    nomi[j + 1] = t;
+                    t1 = posizione[j];
+                    posizione[j] = posizione[j + 1];
+                    posizione[j + 1] = t1;
+                    scambio = true;
+                }
+                j = j + 1;
+            }
+            i = i + 1;
+        }
+    }
+
+        public static void VisualizzaVettore (int[] nomi,int[] posizione ,N){ 
+            int i;
+            i=0;
+            while (i<N){
+                System.out.println("La scuderia"+nomi[i]+"si trova in posizione:"+posizione[i]);
+                i++;
+            }
+
+        }
+
+            
+
+        public static void impostaVettoreRandom e (int[] nomi,int[] posizione ,N){
+            int i;
+            i=0;
+            while (i<N){
+                posizione[i]=random(N*10 + 1);
+                posizioni[i]=nome[i];
+                System.out.println("La scuderia"+nomi[i]+"si trova in posizione:"+posizione[i])
+                i++;
+            }
+
+
+        
+        }
+
+            
+
+
+    
+
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
 
-        //Creo l'oggetto in per l'input da tastiera
         Scanner in = new Scanner( System.in );
+        int n, i;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        System.out.println("Inserisci il numero di scuderie");
+        n = input.nextInt();
+        String[] nomi = new String[n];
+        int[] posizione = new int[n];
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        i = 0;
+        while (i < n) {
+            nomi[0] = Ferrari;
+            nomi[1] = RedBull;
+            nomi[2] = Mclaren;
+            nomi[3] = AstonMartin;
+            nomi[4] = Sauber;
+            nomi[5] = Williams;
+            nomi[6] = AstonMartin;
+            nomi[7] = Haas;
+            nomi[8] = Alpine;
+            nomi[9] = RacingBull;
+        }
+        System.out.println("Genera le posizioni");
+        impostaVettoreRandom(posizione, n, nomi);
+        System.out.println("posizioni generate");
+        visualizzaVettore(nomi, posizione, n);
+        System.out.println("Ordinamento posizioni in corso");
+        bubblesortparalleli(nomi, posizione, n);
+        System.out.println("Tabellone gara con posizioni ordinate");
+        visualizzaVettore(nomi, posizione, n);
+
     }
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
